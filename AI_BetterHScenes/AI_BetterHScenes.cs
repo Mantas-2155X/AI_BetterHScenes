b864947c5a36eb02273a21138bb44e619b98cb23
Mantas <syenergyy@gmail.com>
2019-12-15 18:11:32 +0200

    Added retain cum after H

--- AI_BetterHScenes/AI_BetterHScenes.cs
+++ AI_BetterHScenes/AI_BetterHScenes.cs
@@ -1,15 +1,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 
 using HarmonyLib;
 
 using BepInEx;
+using BepInEx.Logging;
 using BepInEx.Harmony;
 using BepInEx.Configuration;
 
 using AIProject;
+using AIChara;
+using AIProject.Definitions;
 using Manager;
+
 using UnityEngine;
 
 namespace AI_BetterHScenes
@@ -17,11 +22,15 @@ namespace AI_BetterHScenes
     [BepInPlugin(nameof(AI_BetterHScenes), nameof(AI_BetterHScenes), VERSION)][BepInProcess("AI-Syoujyo")]
     public class AI_BetterHScenes : BaseUnityPlugin
     {
-        public const string VERSION = "1.2.1";
+        public const string VERSION = "1.3.1";
+
+        private new static ManualLogSource Logger;
 
         private static bool inHScene;
 
         private static VirtualCameraController hCamera;
+
+        private static List<ChaControl> shouldCleanUp;
         
         private static GameObject map;
         private static GameObject mapSimulation;
@@ -30,6 +39,7 @@ namespace AI_BetterHScenes
         private static bool mapShouldEnable; // compatibility with other plugins which might disable the map
         private static bool mapSimulationShouldEnable; // compatibility with other plugins which might disable the map simulation
         
+        private static ConfigEntry<bool> retainCumAfterH { get; set; }
         private static ConfigEntry<bool> keepButtonsInteractive { get; set; }
         private static ConfigEntry<int> hPointSearchRange { get; set; }
         private static ConfigEntry<bool> forceTearsOnWeakness { get; set; }
@@ -41,8 +51,17 @@ namespace AI_BetterHScenes
         private static ConfigEntry<bool> disableMapSimulation { get; set; }
         private static ConfigEntry<bool> optimizeCollisionHelpers { get; set; }
         
+        private static float Remap(float value, float from1, float to1, float from2, float to2) {
+            return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+        }
+        
         private void Awake()
         {
+            Logger = base.Logger;
+
+            shouldCleanUp = new List<ChaControl>();
+            
+            retainCumAfterH = Config.Bind("QoL", "Keep cum on body after H", false, new ConfigDescription("Keep cum on body after H, will clean up if taking a bath or changing clothes"));
             keepButtonsInteractive = Config.Bind("QoL", "Keep UI buttons interactive", true, new ConfigDescription("Keep buttons interactive during certain events like orgasm"));
             hPointSearchRange = Config.Bind("QoL", "H point search range", 60, new ConfigDescription("Range in which H points are shown when changing location", new AcceptableValueRange<int>(1, 1000)));
             forceTearsOnWeakness = Config.Bind("QoL", "Tears when weakness is reached", true, new ConfigDescription("Make girl cry when weakness is reached during H"));
@@ -124,11 +143,121 @@ namespace AI_BetterHScenes
             HarmonyWrapper.PatchAll(typeof(AI_BetterHScenes));
         }
 
+        //-- Add character to the shouldCleanUp list --//
+        [HarmonyPostfix, HarmonyPatch(typeof(SiruPasteCtrl), "Proc")]
+        public static void SiruPasteCtrl_Proc_PopulateList(SiruPasteCtrl __instance)
+        {
+            if (!inHScene || !retainCumAfterH.Value || shouldCleanUp == null)
+                return;
+
+            ChaControl chara = Traverse.Create(__instance).Field("chaFemale").GetValue<ChaControl>();
+            if (chara == null || shouldCleanUp.Contains(chara))
+                return;
+
+            AgentActor agent = Singleton<Manager.Map>.Instance.AgentTable.Values.FirstOrDefault(actor => actor != null && actor.ChaControl == chara);
+            if (agent == null)
+                return;
+            
+            for (int i = 0; i < 5; i++)
+            {
+                ChaFileDefine.SiruParts parts = (ChaFileDefine.SiruParts)i;
+                
+                if (chara.GetSiruFlag(parts) == 0) 
+                    continue;
+                
+                shouldCleanUp.Add(chara);
+                
+                int bathDesireType = Desire.GetDesireKey(Desire.Type.Bath);
+                int lewdDesireType = Desire.GetDesireKey(Desire.Type.H);
+
+                float clampedReason = Remap(agent.GetFlavorSkill(FlavorSkill.Type.Reason), 0, 99999f, 0, 100f);
+                float clampedDirty = Remap(agent.GetFlavorSkill(FlavorSkill.Type.Dirty), 0, 99999f, 0, 100f);
+                float clampedLewd = agent.GetDesire(lewdDesireType) ?? 0;
+                float newBathDesire = 100f + clampedReason - clampedDirty - clampedLewd * 1.25f;
+                
+                agent.SetDesire(bathDesireType, Mathf.Clamp(newBathDesire, 0f, 100f));
+                break;
+            }
+        }
+        
+        //-- Clean up chara after bath if retaining cum effect --//
+        [HarmonyPostfix, HarmonyPatch(typeof(Bath), "OnCompletedStateTask")]
+        public static void Bath_OnCompletedStateTask_CleanUp(Bath __instance) => CleanUpSiru(__instance);
+        
+        //-- Clean up chara after changing if retaining cum effect --//
+        [HarmonyPostfix, HarmonyPatch(typeof(ClothChange), "OnCompletedStateTask")]
+        public static void ClothChange_OnCompletedStateTask_CleanUp(ClothChange __instance) => CleanUpSiru(__instance);
+        
+        //-- Retain siru state EndProc--//
+        [HarmonyTranspiler, HarmonyPatch(typeof(HScene), "EndProc")]
+        public static IEnumerable<CodeInstruction> HScene_EndProc_PreventCleaningSiru(IEnumerable<CodeInstruction> instructions)
+        {
+            var il = instructions.ToList();
+
+            var index = il.FindIndex(instruction => instruction.opcode == OpCodes.Callvirt && (instruction.operand as MethodInfo)?.Name == "SetSiruFlag");
+            if (index <= 0)
+            {
+                Logger.LogMessage("Failed transpiling 'HScene_EndProc_PreventCleaningSiru' SetSiruFlag index not found!");
+                Logger.LogWarning("Failed transpiling 'HScene_EndProc_PreventCleaningSiru' SetSiruFlag index not found!");
+                return il;
+            }
+
+            il[index - 11] = new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(AI_BetterHScenes), nameof(ChangeSiruIndex)));
+            
+            return il;
+        }
+        
+        //-- Retain siru state OnDisable--//
+        [HarmonyTranspiler, HarmonyPatch(typeof(HScene), "OnDisable")]
+        public static IEnumerable<CodeInstruction> HScene_OnDisable_PreventCleaningSiru(IEnumerable<CodeInstruction> instructions)
+        {
+            var il = instructions.ToList();
+
+            var index = il.FindIndex(instruction => instruction.opcode == OpCodes.Callvirt && (instruction.operand as MethodInfo)?.Name == "SetSiruFlag");
+            if (index <= 0)
+            {
+                Logger.LogMessage("Failed transpiling 'HScene_OnDisable_PreventCleaningSiru' SetSiruFlag index not found!");
+                Logger.LogWarning("Failed transpiling 'HScene_OnDisable_PreventCleaningSiru' SetSiruFlag index not found!");
+                return il;
+            }
+
+            il[index - 8] = new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(AI_BetterHScenes), nameof(ChangeSiruIndex)));
+            
+            return il;
+        }
+
+        private static int ChangeSiruIndex()
+        {
+            return retainCumAfterH.Value ? 5 : 0;
+        }
+        
+        private static void CleanUpSiru(AgentStateAction __instance)
+        {
+            if (shouldCleanUp == null || shouldCleanUp.Count == 0 || __instance == null || __instance.Owner == null)
+                return;
+            
+            var tree = (__instance.Owner as AgentBehaviorTree);
+            if (tree == null)
+                return;
+
+            AgentActor agent = tree.SourceAgent;
+            if (agent == null || agent.ChaControl == null || !shouldCleanUp.Contains(agent.ChaControl))
+                return;
+
+            for (int i = 0; i < 5; i++)
+            {
+                ChaFileDefine.SiruParts parts = (ChaFileDefine.SiruParts)i;
+                agent.ChaControl.SetSiruFlag(parts, 0);
+            }
+
+            shouldCleanUp.Remove(agent.ChaControl);
+        }
+
         //-- Strip male/futa pants when starting H --//
         //-- Strip male/futa pants when changing animation --//
-        public static void HScene_StartAnim_StripMalePants() => HScene_StripMalePants(stripMalePantsStartH.Value);
-        public static void HScene_ChangeAnimation_StripMalePants() => HScene_StripMalePants(stripMalePantsChangeAnim.Value);
-        public static void HScene_StripMalePants(bool shouldStrip)
+        private static void HScene_StartAnim_StripMalePants() => HScene_StripMalePants(stripMalePantsStartH.Value);
+        private static void HScene_ChangeAnimation_StripMalePants() => HScene_StripMalePants(stripMalePantsChangeAnim.Value);
+        private static void HScene_StripMalePants(bool shouldStrip)
         {
             if (!shouldStrip || !Singleton<HSceneManager>.IsInstance())
                 return;
@@ -137,7 +266,7 @@ namespace AI_BetterHScenes
             if (manager == null || manager.Player == null)
                 return;
 
-            AIChara.ChaControl ply = manager.Player.ChaControl;
+            ChaControl ply = manager.Player.ChaControl;
             if ((ply.sex == 0 || manager.bFutanari) && ply.IsClothesStateKind(1))
                 ply.SetClothesState(1, 2);
         }
@@ -212,14 +341,14 @@ namespace AI_BetterHScenes
             inHScene = false;
             
             if(map != null)
-                if (disableMap.Value || mapShouldEnable)
+                if (mapShouldEnable)
                 {
                     map.SetActive(true);
                     mapShouldEnable = false;
                 }
             
             if(mapSimulation != null)
-                if (disableMapSimulation.Value || mapSimulationShouldEnable)
+                if (mapSimulationShouldEnable)
                 {
                     mapSimulation.SetActive(true);
                     mapSimulationShouldEnable = false;
